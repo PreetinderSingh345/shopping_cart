a4c3d2b47c4e6e0c94d61ab361365b949d9d1fb8
Preetinder Singh <preetindersingh072@gmail.com>
2021-01-01 13:01:39 +0530

    setup firebase configuration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-// importing react, react dom, index styling and the App component
+// importing react, react dom, index styling, App component, firebase and firestore
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from "firebase";
+import "firebase/firestore"
+
+// defining firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBu8eNJ9USJYO8WSV3N1qeIn7-CfPxZmu0",
+  authDomain: "cart-8e29b.firebaseapp.com",
+  projectId: "cart-8e29b",
+  storageBucket: "cart-8e29b.appspot.com",
+  messagingSenderId: "1095169277054",
+  appId: "1:1095169277054:web:15af663d3db7b336602c2f"
+
+};
+
+// initializing firebase configuration
+
+firebase.default.initializeApp(firebaseConfig);
 
 // telling react dom to render the App component as the root element
 
